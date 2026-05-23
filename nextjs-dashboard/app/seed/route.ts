91a964879db51b7c12cb5787fe31c968ce8fa199
wmunoa86 <wmunoa@icloud.com>
2026-05-22 22:05:50 -0600

    route file

--- nextjs-dashboard/app/seed/route.ts
+++ nextjs-dashboard/app/seed/route.ts
@@ -103,12 +103,10 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
